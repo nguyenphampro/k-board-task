778b5a8d2b3560a088041ee9891bc5010f6adff0
Bảo Nguyên <baonguyenyam@gmail.com>
2018-06-16 10:57:05 +0700

    add data demo

--- src/_footer.js
+++ src/_footer.js
@@ -1,1 +1,1 @@
-var OYsGZnhhsu =  "\u003Cfooter class=\"footer text-center\"\u003E\u003Cdiv class=\"container\"\u003E\u003Cspan class=\"text-muted small\"\u003ECopyright by Bao Nguyen\u003C\u002Fspan\u003E\u003C\u002Fdiv\u003E\u003C\u002Ffooter\u003E";;document.write(OYsGZnhhsu);;
+var MOTMHDckMp =  "\u003Cfooter class=\"footer text-center\"\u003E\u003Cdiv class=\"container\"\u003E\u003Cspan class=\"text-muted small\"\u003ECopyright by Bao Nguyen\u003C\u002Fspan\u003E\u003C\u002Fdiv\u003E\u003C\u002Ffooter\u003E";;document.write(MOTMHDckMp);;
